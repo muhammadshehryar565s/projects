ca8a40b8d123c17dda4f7ace46de3598bb41a17d
muhammadshehryar565s <123178301+muhammadshehryar565s@users.noreply.github.com>
2024-02-08 12:22:21 +0500

    Delete forApi directory

--- forApi/appname/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Company,Employee,Ouruser
-
-admin.site.register(Company)
-admin.site.register(Employee)
-admin.site.register(Ouruser)
